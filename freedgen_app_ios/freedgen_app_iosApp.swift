04313c0b1bf4ebb057b32e7d0045fc974a1d6426
dhave Barsalote <dhave.barsalote@peddlr.io>
2024-01-03 09:43:14 +0800

    Initial Commit

---
+++ freedgen_app_ios/freedgen_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  freedgen_app_iosApp.swift
+//  freedgen_app_ios
+//
+//  Created by Dhave Barsalote on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct freedgen_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
